deafef00935c27fc74f78ce21f3a98592ba8bbbc
David Waite <davidwaite122@gmail.com>
2024-07-27 13:31:55 +1000

    added 2048

--- .history/app/page_20230726112452.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Hello, hello. I'm David</h1>
-    </main>
-  );
-}
